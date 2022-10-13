0b610b8401b425cbd42a177660d94f378bce2179
Andrei Lazarescu <lazarescu.andrei19@gmail.com>
2022-10-13 18:59:42 +0300

    feat(appconfig_derive): :sparkles: Allow nested configs

--- appconfig_derive/impl/src/lib.rs
+++ appconfig_derive/impl/src/lib.rs
@@ -3,11 +3,14 @@ use std::{collections::HashMap, fmt::Display};
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, TokenStream as TokenStream2};
 use quote::quote;
-use syn::{parse_macro_input, punctuated::Punctuated, ExprAssign, Field, Lit};
+use syn::{parse_macro_input, punctuated::Punctuated, Expr, ExprAssign, Field, Lit};
 
 const NAME: &str = "name";
 const DEFAULT: &str = "default";
 const DEFAULT_FN: &str = "default_fn";
+const NESTED: &str = "nested";
+const SKIP: &str = "skip";
+const PREFIX: &str = "prefix";
 
 enum MyLit {
     Lit(Lit),
@@ -22,11 +25,11 @@ impl Display for MyLit {
             Lit::Str(s) => write!(f, "{}", s.value()),
             Lit::Int(i) => write!(f, "{}", i.base10_digits()),
             Lit::Bool(b) => write!(f, "{}", b.value),
-            Lit::ByteStr(s) => todo!("ByteStr"),
             Lit::Byte(b) => write!(f, "{}", b.value()),
             Lit::Char(c) => write!(f, "{}", c.value()),
             Lit::Float(fl) => write!(f, "{}", fl.base10_digits()),
             Lit::Verbatim(l) => write!(f, "{}", l.to_string()),
+            Lit::ByteStr(_) => todo!("ByteStr"),
         }
     }
 }
@@ -65,19 +68,30 @@ fn parse_attrs<T>(fields: &Punctuated<Field, T>) -> HashMap<String, HashMap<Stri
         };
         let tts = split_tts(g.stream());
         for tt in tts {
-            let res: Result<ExprAssign, syn::Error> = syn::parse2(tt);
-            if res.is_ok() {
-                let expr = res.unwrap();
-                let key = match *expr.left {
-                    syn::Expr::Path(p) => p.path.get_ident().unwrap().to_string(),
-                    _ => panic!("expected path"),
-                };
-                let value = match *expr.right {
-                    syn::Expr::Lit(l) => MyLit::Lit(l.lit).to_string(),
-                    syn::Expr::Path(p) => p.path.get_ident().unwrap().to_string(),
-                    _ => panic!("expected literal"),
-                };
-                field_attrs.insert(key, value);
+            let res: Result<Expr, syn::Error> = syn::parse2(tt);
+            if res.is_err() {
+                eprintln!("err: {}", res.unwrap_err());
+                continue;
+            }
+            let expr = res.unwrap();
+            match expr {
+                Expr::Assign(ExprAssign { left, right, .. }) => {
+                    let key = match *left {
+                        syn::Expr::Path(p) => p.path.get_ident().unwrap().to_string(),
+                        _ => panic!("expected path"),
+                    };
+                    let value = match *right {
+                        syn::Expr::Lit(l) => MyLit::Lit(l.lit).to_string(),
+                        syn::Expr::Path(p) => p.path.get_ident().unwrap().to_string(),
+                        _ => panic!("expected literal"),
+                    };
+                    field_attrs.insert(key, value);
+                }
+                Expr::Path(p) => {
+                    let key = p.path.get_ident().unwrap().to_string();
+                    field_attrs.insert(key, String::new());
+                }
+                _ => {}
             }
         }
         res.insert(f.ident.as_ref().unwrap().to_string(), field_attrs);
@@ -101,14 +115,31 @@ pub fn app_config(input: TokenStream) -> TokenStream {
 
     // TODO: Implement the following attrs:
     // - config(sep = "...")
-    // - config(prefix = "...")
     // - config(datasource = "...")
     let attrs = parse_attrs(&fields);
 
     // TODO: Implement special cases for the `std::collections` types, as well as `Option` and `AppConfig`
+    let basic_fields = fields.iter().filter(|f| {
+        let name = f.ident.as_ref().unwrap().to_string();
+        attrs
+            .get(&name)
+            .map_or(true, |a| !a.contains_key(NESTED) && !a.contains_key(SKIP))
+    });
+
+    let nested_fields = fields.iter().filter(|f| {
+        let name = f.ident.as_ref().unwrap().to_string();
+        attrs
+            .get(&name)
+            .map_or(false, |a| a.contains_key(NESTED) && !a.contains_key(SKIP))
+    });
+
+    let skipped_fields = fields.iter().filter(|f| {
+        let name = f.ident.as_ref().unwrap().to_string();
+        attrs.get(&name).map_or(false, |a| a.contains_key(SKIP))
+    });
 
     // 1. Generate a `builder` struct, with all optional fields
-    let optionized = fields.iter().map(|f| {
+    let optionized = basic_fields.clone().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
         quote! {
@@ -117,23 +148,30 @@ pub fn app_config(input: TokenStream) -> TokenStream {
     });
 
     // 2. Assert that all the types implement either (FromStr and ToString) or AppConfig
-    let assert_types = fields.iter().map(|f| {
+    let assert_types_basic = basic_fields.clone().map(|f| {
         let ty = &f.ty;
         quote! {
-            static_assertions::assert_impl_any!(#ty: std::str::FromStr, appconfig_derive::AppConfig);
-            static_assertions::assert_impl_any!(#ty: std::string::ToString, appconfig_derive::AppConfig);
+            static_assertions::assert_impl_all!(#ty: std::str::FromStr, std::string::ToString);
         }
     });
 
+    let assert_types_nested = nested_fields.clone().map(|f| {
+        let ty = &f.ty;
+        quote! {
+            static_assertions::assert_impl_all!(#ty: appconfig_derive::AppConfig);
+        }
+    });
+
+    let assert_types = assert_types_basic.chain(assert_types_nested);
+
     // 3. Try to load the values from the data source
-    // 3a. TODO: You should be able to specify a name for the key
-    let read_from_data_src = fields.iter().map(|f| {
+    let read_from_data_src = basic_fields.clone().map(|f| {
         let name = &f.ident.as_ref().unwrap();
         let sname = name.to_string();
         let ty = &f.ty;
         let key = attrs.get(&sname).map(|m| m.get(NAME)).flatten().or(Some(&sname)).unwrap().to_uppercase();
         quote! {
-            match data_src.get(#key) {
+            match data_src.get(&(prefix.clone().unwrap_or("".to_string()) + #key)) {
                 Err(e) => return Err(appconfig_derive::AppConfigError::DatastoreError(e)),
                 Ok(None) => {},
                 Ok(Some(value)) => {
@@ -145,7 +183,7 @@ pub fn app_config(input: TokenStream) -> TokenStream {
     });
 
     // 4. Try to load the values from the environment
-    let read_from_env = fields.iter().map(|f| {
+    let read_from_env = basic_fields.clone().map(|f| {
         let name = &f.ident.as_ref().unwrap();
         let sname = name.to_string();
         let ty = &f.ty;
@@ -153,7 +191,7 @@ pub fn app_config(input: TokenStream) -> TokenStream {
 
         quote! {
             builder.#name = builder.#name.or(
-                std::env::var(#key)
+                std::env::var(&(prefix.clone().unwrap_or("".to_string()) + #key))
                     .ok()
                     .map(|value| value.parse::<#ty>().map_err(|e| appconfig_derive::AppConfigError::ParsingError(Box::new(e))))
                     .transpose()?
@@ -162,7 +200,7 @@ pub fn app_config(input: TokenStream) -> TokenStream {
     });
 
     // 5. Try to load the values from the default option
-    let read_from_default = fields.iter().filter_map(|f| {
+    let read_from_default = basic_fields.clone().filter_map(|f| {
         let name = &f.ident.as_ref().unwrap();
         let sname = name.to_string();
         let ty = &f.ty;
@@ -185,16 +223,58 @@ pub fn app_config(input: TokenStream) -> TokenStream {
         })
     });
 
+    let read_fields = read_from_data_src
+        .chain(read_from_env)
+        .chain(read_from_default)
+        .chain(read_from_default_fn);
+
     // 6. Generate the `build` method
     // 6a. TODO: Error out if any required fields are missing
-    let create_fields = fields.iter().map(|f| {
+    let create_basic_fields = basic_fields.clone().map(|f| {
         let name = &f.ident;
         quote! {
-            #name: builder.#name.unwrap_or_default()
+            #name: builder.#name.unwrap()
         }
     });
 
+    let create_nested_fields = nested_fields.clone().map(|f| {
+        let name = &f.ident.as_ref().unwrap();
+        let sname = name.to_string();
+        let ty = &f.ty;
+        let prefix = attrs
+            .get(&sname)
+            .map(|m| m.get(PREFIX))
+            .flatten()
+            .or(Some(&(sname + "_")))
+            .unwrap()
+            .to_uppercase();
+        quote! {
+            #name: #ty::build(data_src, Some(#prefix.to_string()))?
+        }
+    });
+
+    let create_skipped_fields = skipped_fields.clone().map(|f| {
+        let name = &f.ident;
+        quote! {
+            #name,
+        }
+    });
+
+    let create_fields = create_basic_fields
+        .chain(create_nested_fields)
+        .chain(create_skipped_fields);
+
     // 7. TODO: Save fields to data store
+
+    // 8. Read data from build params if skipped
+    let extra_args = skipped_fields.clone().map(|f| {
+        let name = &f.ident; // TODO: This should work with `name`
+        let ty = &f.ty;
+        quote! {
+            , #name: #ty
+        }
+    });
+
     let out = quote! {
         #[derive(Default)]
         pub struct #name {
@@ -204,13 +284,10 @@ pub fn app_config(input: TokenStream) -> TokenStream {
         impl appconfig_derive::AppConfig for #orig_name {}
 
         impl #orig_name {
-            pub fn build(data_src: &mut impl appconfig_derive::DataSource) -> Result<Self, appconfig_derive::AppConfigError> {
+            pub fn build(data_src: &mut impl appconfig_derive::DataSource, prefix: Option<String> #(#extra_args)*) -> Result<Self, appconfig_derive::AppConfigError> {
                 #(#assert_types)*
                 let mut builder = #name::default();
-                #(#read_from_data_src)*
-                #(#read_from_env)*
-                #(#read_from_default)*
-                #(#read_from_default_fn)*
+                #(#read_fields)*
                 Ok(Self {
                     #(#create_fields),*
                 })
--- appconfig_derive/tests/test.rs
+++ appconfig_derive/tests/test.rs
@@ -37,7 +37,7 @@ mod tests {
         let mut data_src = MockDataSource::new();
         data_src.set("FIELD", "hello").unwrap();
 
-        let config = ConfigStr::build(&mut data_src).unwrap();
+        let config = ConfigStr::build(&mut data_src, None).unwrap();
         assert_eq!(config.field, "hello");
     }
 
@@ -46,19 +46,27 @@ mod tests {
         let mut data_src = MockDataSource::new();
         std::env::set_var("FIELD", "hello");
 
-        let config = ConfigStr::build(&mut data_src).unwrap();
+        let config = ConfigStr::build(&mut data_src, None).unwrap();
         assert_eq!(config.field, "hello");
     }
 
     #[test]
     fn ds_takes_precedence() {
         let mut data_src = MockDataSource::new();
-        data_src.set("FIELD", "hello").unwrap();
-        std::env::set_var("FIELD", "world");
+        data_src.set("FIELD", "world").unwrap();
+        std::env::set_var("FIELD", "hello");
 
-        let config = ConfigStr::build(&mut data_src).unwrap();
+        let config = ConfigStr::build(&mut data_src, None).unwrap();
+        assert_eq!(config.field, "world");
+    }
+
+    #[test]
+    fn it_uses_prefix() {
+        let mut data_src = MockDataSource::new();
+        data_src.set("PREFIX_FIELD", "hello").unwrap();
+
+        let config = ConfigStr::build(&mut data_src, Some("PREFIX_".to_string())).unwrap();
         assert_eq!(config.field, "hello");
-        std::env::remove_var("FIELD");
     }
 
     #[derive(AppConfig)]
@@ -74,7 +82,7 @@ mod tests {
         let mut data_src = MockDataSource::new();
         data_src.set("ATTR_FIELD", "hello").unwrap();
 
-        let config = ConfigAttrs::build(&mut data_src).unwrap();
+        let config = ConfigAttrs::build(&mut data_src, None).unwrap();
         assert_eq!(config.field, "hello");
     }
 
@@ -82,7 +90,7 @@ mod tests {
     fn it_reads_default_from_attrs() {
         let mut data_src = MockDataSource::new();
 
-        let config = ConfigAttrs::build(&mut data_src).unwrap();
+        let config = ConfigAttrs::build(&mut data_src, None).unwrap();
         assert_eq!(config.field, "default");
         assert_eq!(config.field2, 4);
     }
@@ -101,7 +109,60 @@ mod tests {
     fn it_reads_default_from_fn() {
         let mut data_src = MockDataSource::new();
 
-        let config = ConfigDefaultFn::build(&mut data_src).unwrap();
+        let config = ConfigDefaultFn::build(&mut data_src, None).unwrap();
         assert_eq!(config.field3, 4);
+    }
+
+    #[derive(AppConfig)]
+    pub struct ConfigSkip {
+        #[appconfig(skip)]
+        field: String,
+    }
+
+    #[test]
+    fn it_skips_fields() {
+        let mut data_src = MockDataSource::new();
+        data_src.set("FIELD", "hello").unwrap();
+
+        let config = ConfigSkip::build(&mut data_src, None, "world".to_string()).unwrap();
+        assert_eq!(config.field, "world");
+    }
+
+    #[derive(AppConfig)]
+    pub struct ConfigNested {
+        field4: i64,
+    }
+
+    #[derive(AppConfig)]
+    pub struct ConfigNested2 {
+        field5: i64,
+        #[appconfig(nested)]
+        field6: ConfigNested,
+    }
+
+    #[test]
+    fn it_reads_nested() {
+        let mut data_src = MockDataSource::new();
+        data_src.set("FIELD5", "5").unwrap();
+        data_src.set("FIELD6_FIELD4", "6").unwrap();
+
+        let config = ConfigNested2::build(&mut data_src, None).unwrap();
+        assert_eq!(config.field5, 5);
+        assert_eq!(config.field6.field4, 6);
+    }
+
+    #[derive(AppConfig)]
+    pub struct ConfigNested3 {
+        #[appconfig(nested, prefix = "nested_")]
+        field7: ConfigNested,
+    }
+
+    #[test]
+    fn it_reads_nested_with_prefix() {
+        let mut data_src = MockDataSource::new();
+        data_src.set("NESTED_FIELD4", "7").unwrap();
+
+        let config = ConfigNested3::build(&mut data_src, None).unwrap();
+        assert_eq!(config.field7.field4, 7);
     }
 }
